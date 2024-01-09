a06bc6443927b5634479e669ee9c9b92577693f5
Pedro Reis <ph3dror3is@gmail.com>
2024-01-09 18:30:41 -0300

    Nova versao de CountChars com ordenacao tb por comprimento das linhas

--- src/CountChars.java
+++ src/CountChars.java
@@ -4,11 +4,19 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
 
-public class CountChars {
-    
+public class CountChars {       
+       
     public static void main(String[] args) throws IOException {
+        
+        final List<int[]> sortedList = new LinkedList<>();   
+        
+        boolean sortedByLength = (args.length == 0);
         
         File dirCurrent = new File(".");
         
@@ -21,6 +29,8 @@ public class CountChars {
             }
         );
         
+        StringBuilder sb;        
+        
         for (String filename: filenames) {
             
             Path path = Path.of(filename);
@@ -29,30 +39,62 @@ public class CountChars {
             
             Scanner scanner = new Scanner(content);
             
-            StringBuilder sb = new StringBuilder(1024);
+            sb = new StringBuilder(1024);     
             
-            int countLines = 1;
+            int countLines = 0;
             
             while (scanner.hasNext()) {
                 
                 String line = scanner.nextLine();
                 
-                sb.append(
-                    String.format(
-                        "% 6d] %,d\n", 
-                        countLines++,
-                        line.length()
-                    )
-                );
+                int[] data = new int[2];
+                
+                countLines++;
+                        
+                if (sortedByLength) { 
+                    data[0] = line.length(); data[1] = countLines;                   
+                    
+                }
+                else {                    
+                    data[0] = countLines; data[1] = line.length(); 
+                }
+               
+                sortedList.add(data);
             }
+ 
+          
+            sb = new StringBuilder(1024);
+            
+            if (sortedByLength) {
+                Collections.sort(sortedList, new Compare()); 
+                sb.append("comprimento : linha\n");
+            }                
+            else 
+                sb.append("      linha : comprimento\n"); 
+
+            
+            for (int[] data : sortedList) {       
+
+                sb.append(String.format("% 11d : %d \n", data[0], data[1]));
+            }            
             
             try (PrintWriter pw = new PrintWriter(filename + ".txt")) {
                 
                 pw.print(sb.toString());
                 
-            }          
-        }
+            }   
+            
+        }//for
            
     }//main
     
-}
+    private static final class Compare implements Comparator<int[]> {
+
+        @Override
+        public int compare(int[] a, int[] b) {
+            
+            return ((int[])a)[0] - ((int[])b)[0];
+        }        
+    }
+    
+}//classe CountChars
